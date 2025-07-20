15a6c6b5ee54ed00526c4011d29a3835153bdc21
FranklinOfimatic <86037750+franklincaza@users.noreply.github.com>
2025-07-20 16:33:09 -0500

    implementacion Entidades del dominio

---
+++ ZombieDefense.Domain/Entities/Zombie.cs
@@ -0,0 +1,13 @@
+namespace ZombieDefense.Domain.Entities
+{
+    public class Zombie
+    {
+        // Entidad Zombie en blanco por ahora
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public int TiempoDisparo { get; set; }
+        public int Puntaje { get; set; }
+        public int NivelAmenaza { get; set; }
+
+    }
+}
